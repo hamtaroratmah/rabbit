ae8da1763e7fe46324e7782b7ade95f12cdedcc8
soulaymanegharroudi <90785038+soulaymanegharroudi@users.noreply.github.com>
2022-12-15 21:13:01 +0100

    to desktop

--- src/controllers/ChallengesController.ts
+++ src/controllers/ChallengesController.ts
@@ -1,15 +1,34 @@
 import {Services} from "../services/services";
+import {customAlphabet, nanoid} from "nanoid";
 
 export class ChallengesController {
 
-  constructor() {
-  }
+  private nanoid:any;
+	constructor() {
+    try {
+      this.nanoid = customAlphabet("1234567890abcdef", 5);
+    }catch (e:any) {
+     throw e.message;
+    }
+	}
 
-  private service = new Services();
+	private service = new Services();
 
-  public async getAllChallenges() {
-    const response = await this.service.getAllChallenges()
-    return response;
-  }
+	public async getAllChallenges() {
+		const response = await this.service.getAllChallenges()
+		return response;
+	}
+
+	public async createChallenge(title: string, description: string, start: Date, end: Date,
+	                             objectif: number, private_: boolean, id_activity: string) {
+    let code;
+    try{
+      code = nanoid()
+    }catch (e:any) {
+     throw e.message;
+    }
+		const response = await this.service.createChallenge(title, description, start, end, objectif, private_, id_activity, code);
+		return response;
+	}
 
 }
--- src/services/services.ts
+++ src/services/services.ts
@@ -35,16 +35,36 @@ export class Services {
 		const {data: challenges, error: errorChallenges} = await supabase
 		.from('challenges')
 		.select('*');
+		if (challenges === null) return challenges;
 		const {data: creator, error: errorUsername} = await supabase
-			.from('profiles')
-			.select('username')
-			.eq('id', challenges[0].id_creator)
-		challenges[0].creator=creator[0].username
+		.from('profiles')
+		.select('username')
+		.eq('id', challenges[0].id_creator)
+		challenges[0].creator = creator ? creator[0].username : "un inconnu";
 		if (errorChallenges || errorUsername) {
 			return null
 		} else {
 			return challenges
 		}
+	}
+
+	public async createChallenge(title: string, description: string, start: Date, end: Date,
+	                             objectif: number, private_: boolean, id_activity: string,
+	                             code: string) {
+		const {data: challenge, error: errorChallenge} = await supabase
+		.from('challenges')
+		.insert({
+			title: title,
+			description: description,
+			start: start,
+			end: end,
+			objectif: objectif,
+			private: private_,
+			code: code,
+			id_activity: id_activity,
+		})
+		.select();
+		console.log(challenge);
 	}
 
 }
