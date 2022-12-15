73290075ac3f79c684937b033e9c32185501043d
Chaymae <chaymae.hbat@student.vinci.be>
2022-12-15 18:07:22 +0100

    add:ActivityDetailsScreen

--- src/App.tsx
+++ src/App.tsx
@@ -11,39 +11,44 @@ import FormCreateChallenge from "./screens/FormCreateChallenge";
 import JoinChallengeScreen from "./screens/JoinChallengeScreen";
 import HomePageScreenUser from "./screens/HomePageScreenUser";
 import HomePageScreenNewUser from "./screens/HomePageScreenNewUser";
-import {useContext, useEffect} from "react";
+import { useContext, useEffect } from "react";
 import supabase from "./utils/Client";
-import {Context as SessionContext} from './contexts/SessionContext';
+import { Context as SessionContext } from "./contexts/SessionContext";
+import ActivityDetailsScreen from "./screens/ActivityDetailsScreen";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const App = () => {
-	// @ts-ignore
-	const {session, defineSession} = useContext(SessionContext);
-	useEffect(() => {
-		supabase.auth.getSession().then(({ data: { session } }) => {
-			defineSession(session);
-		});
+  // @ts-ignore
+  const { session, defineSession } = useContext(SessionContext);
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      defineSession(session);
+    });
 
-		supabase.auth.onAuthStateChange((_event, session) => {
-			defineSession(session);
-		});
-	}, []);
-	return (
-		<NavigationContainer>
-			<Stack.Navigator
-				initialRouteName="Welcome"
-				screenOptions={{headerShown: false}}
-			>
-				<Stack.Screen name="TabNavigator" component={TabNavigator}/>
-				<Stack.Screen name="SignIn" component={SignInScreen}/>
-				<Stack.Screen name="SignUp" component={SignUpScreen}/>
-				<Stack.Screen name="Welcome" component={WelcomeScreen}/>
-				<Stack.Screen name="AddActivityScreen" component={AddActivityScreen}/>
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
-}
+    supabase.auth.onAuthStateChange((_event, session) => {
+      defineSession(session);
+    });
+  }, []);
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="ActivityDetailsScreen"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="TabNavigator" component={TabNavigator} />
+        <Stack.Screen name="SignIn" component={SignInScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="AddActivityScreen" component={AddActivityScreen} />
+        <Stack.Screen
+          name="ActivityDetailsScreen"
+          component={ActivityDetailsScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default App;
